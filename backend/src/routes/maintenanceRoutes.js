cb0f75f6ced747f3e3b53c454be4779452240f80
DaiVy <vyvdde180817@fpt.edu.vn>
2026-07-26 09:09:17 +0700

    chore(BE & FE): Update feature session management for Admin and set status maintance

--- backend/src/routes/maintenanceRoutes.js
+++ backend/src/routes/maintenanceRoutes.js
@@ -4,9 +4,9 @@ const maintenanceController = require("../controllers/maintenanceController");
 const { protect, authorize } = require("../middlewares/authMiddleware");
 
 // Start maintenance
-router.post("/start", protect, authorize("admin", "manager"), maintenanceController.startMaintenance);
+router.post("/start", protect, authorize("admin", "manager", "staff"), maintenanceController.startMaintenance);
 
 // End maintenance
-router.post("/end", protect, authorize("admin", "manager"), maintenanceController.endMaintenance);
+router.post("/end", protect, authorize("admin", "manager", "staff"), maintenanceController.endMaintenance);
 
 module.exports = router;
